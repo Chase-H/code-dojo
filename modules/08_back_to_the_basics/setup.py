a5e7a8fc617800d4fcfb928d2e11e60f3102e996
Chase Hudson <chasehudson@squareup.com>
2023-07-25 10:21:12 -0400

    Added Images

---
+++ modules/08_back_to_the_basics/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='test-module',
+    version='0.1.0',
+    description='Module to illustrate a point',
+    long_description='',
+    author='Chase Hudson',
+    author_email='chasehudson@squareup.com',
+    packages=find_packages(),
+    zip_safe=False,
+    keywords='test_module',
+    classifiers=[],
+)
